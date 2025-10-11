520149854377fab6d2e06cd2563e2da1646e6061
Mr.Professor <wasim.hannure@yahoo.in>
2025-10-11 05:45:34 +0530

    revise git action again

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "manual-rg"
     storage_account_name = "mybackendtfstorage"
-    container_name       = "backend"
+    container_name       = "mytfcontainer"
     key                  = "terraform.tfstate"
   }
 }
